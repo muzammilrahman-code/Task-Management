1802f7f0a94a968bf97f4a8f023a69569da5ca53
Muzammil Rahman <your-email@example.com>
2025-11-06 01:13:52 +0500

    Fixed Sidebar and Navbar import paths

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from '../components/Home/sidebar'
+import Sidebar from '../components/Home/Sidebar'
 import Navbar from '../components/Home/Navbar'
 import { Outlet } from 'react-router-dom';
 const Home = () => {
